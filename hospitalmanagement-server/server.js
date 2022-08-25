dcf2cde17ad8a0156a2b3ae8f26441fb17d82172
nishanthan <nishanthan@codevus.com>
2022-08-25 21:26:36 +0530

    db connection

--- hospitalmanagement-server/server.js
+++ hospitalmanagement-server/server.js
@@ -1,1 +1,18 @@
-ks - nisha;
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = 8000;
+const DB_URL =
+  "mongodb+srv://nishan:nisha123@cluster1.trfvymb.mongodb.net/SPM_DB?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database connected successfully!!!");
+  })
+  .catch((err) => console.log("Database connection error", err));
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
