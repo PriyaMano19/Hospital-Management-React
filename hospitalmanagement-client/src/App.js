3316e3317607ef70226115a267947996dfb232dd
priya_19 <it20141042@my.sliit.lk>
2022-08-31 21:28:55 +0530

    navbar component imported in appjs no need to implement all the pages seperately

--- hospitalmanagement-client/src/App.js
+++ hospitalmanagement-client/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import NavBar from "./componets/NavBar";
+import Dashboard from "./P_Views/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path="/nav" element={<NavBar />} />
+          {/* <Route path="/nav" element={<NavBar />} /> */}
+          <Route path="/labdash" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
