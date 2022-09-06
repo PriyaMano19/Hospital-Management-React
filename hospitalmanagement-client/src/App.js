3b66e1c55783921a04daa2323920fc87a0262a3d
thusara5 <thusaarul2000@gmail.com>
2022-09-06 23:29:36 +0530

    updated

--- hospitalmanagement-client/src/App.js
+++ hospitalmanagement-client/src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
 import NavBar from './componets/NavBar';
-import { BrowserRouter , Routes,Route } from 'react-router-dom';
+import Appointment from './componets/Appointment'
+import Resappointment from './componets/Resappointment';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/common/Navbar";
+import Home from "./components/pages/Home";
+import CrudAdd from "./components/cruds/CrudAdd";
+import CrudTable from "./components/cruds/CrudTable";
+import CrudListView from "./components/cruds/CrudListView";
+import CrudGridView from "./components/cruds/CrudGridView";
+import CrudDetails from "./components/cruds/CrudDetails";
+import CrudEdit from "./components/cruds/CrudEdit";
+import CrudDelete from "./components/cruds/CrudDelete";
+import Footer from "./components/common/Footer";
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-   <Routes>
-    
-   <Route   path='/nav' element={<NavBar/>}/>
-    
-    </Routes>
+      <NavBar />
+      <BrowserRouter>
+      
+      <Routes>
+        
+        <Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/api/" element={<Home />} />
+					<Route exact path="/cruds" element={<CrudTable />} />					
+					<Route exact path="/cruds/list-view" element={<CrudListView />} />
+					<Route exact path="/cruds/grid-view" element={<CrudGridView />} />
+					<Route exact path="/cruds/new" element={<CrudAdd />} />
+					<Route exact path="/cruds/:_id" element={<CrudDetails />} />
+					<Route exact path="/cruds/:_id/edit" element={<CrudEdit />} />
+					<Route exact path="/cruds/:_id/delete" element={<CrudDelete />} />
+				</Routes>
+          
+       
+      </Routes>
     </BrowserRouter>
     </div>
   );
---
+++ hospitalmanagement-client/src/componets/cruds/CrudAdd.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { post } from "axios";
+import { useNavigate } from "react-router-dom";
+import Select from 'react-select';
+
+
+
+function CrudAdd(props) {
+	const initialState = {
+		companyName: "",
+		phone: "",
+		email: "",
+		location: "3.00 PM",
+		link: " ",
+		description: "",
+	};
+	const [crud, setCrud] = useState(initialState);
+
+
+	const Time = ["1.00", "2.00"];
+	
+
+	const navigate = useNavigate();
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		//if (!crud.companyName || !crud.email) return;
+		async function postCrud() {
+			try {
+				const response = await post("/api/cruds/", crud);
+				navigate(`/cruds/grid-view`);
+				// navigate(`/cruds/${response.data._id}`);
+			} catch (error) {
+				console.log("error", error);
+			}
+		}
+		postCrud();
+	}
+
+	function handleChange(event) {
+		setCrud({ ...crud, [event.target.name]: event.target.value });
+	}
+
+	function handleCancel() {
+		navigate("/cruds");
+	}
+
+	return (
+		<div className="container" style={{ maxWidth: "400px" }}>
+			<h1> Appointment </h1>
+			<hr />
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label>Doctor Name</label>
+					<input
+						name="companyName"
+						type="text"
+						required
+						value={crud.companyName}
+						onChange={handleChange}
+						className="form-control"
+					/>
+					<div className="form-group">
+					<label>patient name</label>
+					<input
+						name="description"
+						row="10"
+						value={crud.description}
+						onChange={handleChange}
+						className="form-control"
+					/>
+				</div>
+				</div>
+				<div className="form-group">
+					<label>Phone</label>
+					<input
+						name="phone"
+						type="tel"
+						pattern="(251)-[0-9]{3}-[0-9]{6}"
+						required
+						value={crud.phone}
+						onChange={handleChange}
+						className="form-control"
+					/>
+					<small>Format: 251-XXX-XXXXXX</small>
+				</div>
+				<div className="form-group">
+					<label>Email</label>
+					<input
+						name="email"
+						type="email"
+						pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$"
+						required
+						value={crud.email}
+						onChange={handleChange}
+						className="form-control"
+					/>
+				</div>
+				<div className="form-group">
+					<label>Time</label>
+					<input
+						name="location"
+						type="text"
+						required
+						value={crud.location}
+						onChange={handleChange}
+						className="form-control"
+					/>
+				</div>
+				<div className="form-group">
+					<label>Date</label>
+					<input
+						name="link"
+						type="text"
+						value={crud.link}
+						onChange={handleChange}
+						className="form-control"/>
+					
+	
+					
+				</div>
+
+				
+                      <br/><br/>
+				<div className="btn-group">
+					
+					<input type="submit" value="Submit" className="btn btn-primary" />
+					&nbsp;&nbsp;&nbsp;
+					<button
+						type="button"
+						onClick={handleCancel}
+						className="btn btn-secondary"
+					>
+						Cancel
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default CrudAdd;
