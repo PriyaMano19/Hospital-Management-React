fae0aa2da42012044d37dcfacc6948b16c04c1af
nishanthan <nishanthan@codevus.com>
2022-09-29 00:12:54 +0530

    patients_Pdf

--- hospitalmanagement-client/src/App.js
+++ hospitalmanagement-client/src/App.js
@@ -6,6 +6,9 @@ import EditPatient from "./componets/n_EditPatient";
 import Patient from "./componets/n_DetailPatient";
 import Login from "./componets/n_login/n_Login";
 import Home from "./componets/n_home";
+import Programs from "./componets/n_Programs";
+import PrintPatients from "./componets/n_PrintPatients";
+import CreateProgram from "./componets/n_CreateProgram";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -19,6 +22,8 @@ function App() {
           <Route path="/patient/:id" element={<Patient />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/createProgram" element={<CreateProgram />} />
         </Routes>
       </BrowserRouter>
     </div>
--- hospitalmanagement-client/src/componets/n_Programs.js
+++ hospitalmanagement-client/src/componets/n_Programs.js
@@ -1,82 +1,58 @@
-import React, { useRef, Component } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import { Col, Container, Row } from "react-bootstrap";
 import logo from "../assets/logo.webp";
 import slide1 from "../assets/n_slide1.jpg";
 import slide2 from "../assets/n_slide2.jpg";
 import slide3 from "../assets/n_slide3.jpg";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
 
-export default class n_Patients extends Component {
+export default class n_programs extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      users: [],
+      programs: [],
     };
   }
 
   componentDidMount() {
-    this.retriveUsers();
+    this.retrivePrograms();
   }
 
-  retriveUsers() {
-    axios.get("http://localhost:8000/users").then((res) => {
+  retrivePrograms() {
+    axios.get("http://localhost:8000/programs").then((res) => {
       if (res.data.success) {
         this.setState({
-          users: res.data.existingUsers,
+          programs: res.data.existingPrograms,
         });
 
-        console.log(this.state.users);
+        console.log(this.state.programs);
       }
     });
   }
 
   onDelete = (id) => {
-    axios.delete(`http://localhost:8000/user/delete/${id}`).then((res) => {
+    axios.delete(`http://localhost:8000/program/delete/${id}`).then((res) => {
       alert("Deleted Successfully");
-      this.retriveUsers();
+      this.retrivePrograms();
     });
   };
 
-  handleSearchArea = (e) => {
-    const searchKey = e.currentTarget.value;
+  //   handleSearchArea = (e) => {
+  //     const searchKey = e.currentTarget.value;
 
-    axios.get("http://localhost:8000/users").then((res) => {
-      if (res.data.success) {
-        this.filterData(res.data.existingUsers, searchKey);
-      }
-    });
-  };
+  //     axios.get("http://localhost:8000/programs").then((res) => {
+  //       if (res.data.success) {
+  //         this.filterData(res.data.existingPrograms, searchKey);
+  //       }
+  //     });
+  //   };
 
-  filterData(users, searchKey) {
-    const result = users.filter((user) => user.userName.includes(searchKey));
+  //   filterData(users, searchKey) {
+  //     const result = users.filter((user) => user.userName.includes(searchKey));
 
-    this.setState({ users: result });
-  }
-
-  pdfGeneate = (users) => {
-    var doc = new jsPDF("landscape", "px", "a4", "false");
-    const columns = [
-      { title: "Name", field: "userName" },
-      { title: "Phone", field: "phone" },
-      { title: "Email", field: "email" },
-      { title: "Password", field: "password" },
-      { title: "DoB", field: "dob" },
-    ];
-    const tableRows = [users];
-    doc.addImage(logo, "webp", 20, 3, 30, 30);
-    doc.text(70, 10, "Hospital Zone Patient Details");
-    //doc.addPage();
-    //doc.text(120, 414, "Nishanthan Kanagasunderam");
-    //doc.text(120, 600, `{this.state.users}`);
-    doc.autoTable({
-      columns: columns.map((col) => ({ ...col, dataKey: col.field })),
-      body: this.state.users,
-    });
-    doc.save("Zone_patients.pdf");
-  };
+  //     this.setState({ users: result });
+  //   }
 
   render() {
     return (
@@ -90,7 +66,7 @@ export default class n_Patients extends Component {
             marginTop: "25px",
           }}
         >
-          All Patients
+          All Programs
         </p>
         <div className="col-lg-9 mt-2 mb-2">
           <input
@@ -98,7 +74,7 @@ export default class n_Patients extends Component {
             type="search"
             placeholder="search"
             name="searchbar"
-            onChange={this.handleSearchArea}
+            //onChange={this.handleSearchArea}
             style={{
               width: "250px",
             }}
@@ -112,30 +88,32 @@ export default class n_Patients extends Component {
                   <thead>
                     <tr>
                       <th scope="col">#</th>
-                      <th scope="col">User Name</th>
-                      <th scope="col">Email</th>
-                      <th scope="col">Phone</th>
-                      <th scope="col">Password</th>
-                      <th scope="col">Dob</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Age Group</th>
+                      <th scope="col">Gender</th>
+                      <th scope="col">PasSample</th>
+                      <th scope="col">Desc</th>
+                      <th scope="col">Price</th>
                       <th scope="col">Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {this.state.users.map((users, index) => (
+                    {this.state.programs.map((programs, index) => (
                       <tr key={index}>
                         <th scope="row">{index + 1}</th>
                         <td>
                           <a
-                            href={`http://localhost:8000/user/${users._id}`}
+                            href={`/program/${programs._id}`}
                             style={{ textDecoration: "none" }}
                           >
-                            {users.userName}
+                            {programs.title}
                           </a>
                         </td>
-                        <td>{users.email}</td>
-                        <td>{users.phone}</td>
-                        <td>{users.password}</td>
-                        <td>{users.dob}</td>
+                        <td>{programs.ageGroup}</td>
+                        <td>{programs.gender}</td>
+                        <td>{programs.sample}</td>
+                        <td>{programs.desc}</td>
+                        <td>{programs.price}</td>
                         <td>
                           {/* <a
                     className="btn btn-warning"
@@ -147,7 +125,7 @@ export default class n_Patients extends Component {
                           <a
                             className="btn btn-danger"
                             href="#"
-                            onClick={() => this.onDelete(users._id)}
+                            onClick={() => this.onDelete(programs._id)}
                           >
                             <i className="fas fa-trash"></i>&nbsp;Delete
                           </a>
@@ -157,21 +135,13 @@ export default class n_Patients extends Component {
                   </tbody>
                 </table>
               </Col>
-
               <Col lg={3} md={6} sm={12}>
                 <button
                   className="btn btn-success text-left"
                   style={{ marginRight: "210px" }}
-                  onClick={this.pdfGeneate}
                 >
                   {" "}
-                  <i className="fas fa-print"></i>&nbsp;&nbsp;
-                  <a
-                    className="text-decoration-none text-dark "
-                    href="/patients"
-                  >
-                    Print
-                  </a>
+                  <i className="fas fa-print"></i>&nbsp;&nbsp;Print
                 </button>
                 <br></br>
                 <hr></hr>
